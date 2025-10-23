1e77c65d912a35dde810b9201156bed46b9b0b09
priyanka-bidnal <bidnalpriyanka@gmail.com>
2025-10-23 16:32:04 +0530

    save to bmi

--- body.py
+++ body.py
@@ -2,6 +2,14 @@ weight = float(input("Weight (kg): "))
 height = float(input("Height (m): "))
 
 bmi = weight / (height ** 2) if height > 0 else 0
-print(f"Weight: {weight} kg")
+if bmi < 18.5:
+    category = "Underweight"
+elif bmi < 25:
+    category = "Normal"
+else:
+    category = "Overweight"
+
+print(f"Weight: {int(weight) if weight.is_integer() else weight} kg")
 print(f"Height: {height} m")
 print(f"BMI: {round(bmi,1)}")
+print(f"Category: {category}")
